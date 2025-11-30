dc8fd07f8c1f2aeec5299af8298c2e7b6402b8bd
Miguel Villarraga Franco <143026348+Miguelvillarraga@users.noreply.github.com>
2025-11-30 17:25:10 -0500

    Add files via upload

--- homework/homework.py
+++ homework/homework.py
@@ -4,6 +4,8 @@ Escriba el codigo que ejecute la accion solicitada.
 
 # pylint: disable=import-outside-toplevel
 
+import pandas as pd
+import os
 
 def clean_campaign_data():
     """
@@ -49,9 +51,87 @@ def clean_campaign_data():
 
 
     """
+    input_folder = "files/input/"
+    output_folder = "files/output/"
+    os.makedirs(output_folder, exist_ok=True)
 
-    return
+    client_cols = [
+        "client_id",
+        "age",
+        "job",
+        "marital",
+        "education",
+        "credit_default",
+        "mortgage",
+    ]
+    campaign_cols = [
+        "client_id",
+        "number_contacts",
+        "contact_duration",
+        "previous_campaign_contacts",
+        "previous_outcome",
+        "campaign_outcome",
+        "last_contact_date",
+    ]
+    economics_cols = [
+        "client_id",
+        "cons_price_idx",
+        "euribor_three_months",
+    ]
+
+    client_csv = []
+    campaign_csv = []
+    economics_csv = []
+
+    meses = {
+        "jan": 1,
+        "feb": 2,
+        "mar": 3,
+        "apr": 4,
+        "may": 5,
+        "jun": 6,
+        "jul": 7,
+        "aug": 8,
+        "sep": 9,
+        "oct": 10,
+        "nov": 11,
+        "dec": 12,
+    }
+
+    for i in range(10):
+        file_path = os.path.join(input_folder, f"bank-marketing-campaing-{i}.csv.zip")
+        df = pd.read_csv(file_path, compression="zip")
+        # Clean client data
+        df["job"] = df["job"].str.replace(".", "", regex=False).str.replace("-", "_", regex=False)
+        df["education"] = df["education"].str.replace(".", "_", regex=False).replace("unknown", pd.NA)
+        df["credit_default"] = df["credit_default"].apply(lambda x: 1 if x == "yes" else 0)
+        df["mortgage"] = df["mortgage"].apply(lambda x: 1 if x == "yes" else 0)
+        df["previous_outcome"] = df["previous_outcome"].apply(lambda x: 1 if x == "success" else 0)
+        df["campaign_outcome"] = df["campaign_outcome"].apply(lambda x: 1 if x == "yes" else 0 )
+        df["month"] = df["month"].str.lower().map(meses)
+        df["last_contact_date"] = pd.to_datetime(dict(year=2022, month=df["month"], day=df["day"]))
+        df_1 = df[client_cols].copy()
+        df_2 = df[campaign_cols].copy()
+        df_3 = df[economics_cols].copy()
+        client_csv.append(df_1)
+        campaign_csv.append(df_2)
+        economics_csv.append(df_3)
+    
+    client_csv = pd.concat(client_csv, ignore_index=True)
+    campaign_csv = pd.concat(campaign_csv, ignore_index=True)
+    economics_csv = pd.concat(economics_csv, ignore_index=True)
+    client_csv.to_csv(os.path.join(output_folder, "client.csv"), index=False)
+    campaign_csv.to_csv(os.path.join(output_folder, "campaign.csv"), index=False)
+    economics_csv.to_csv(os.path.join(output_folder, "economics.csv"), index=False)
+
+    return client_csv, campaign_csv, economics_csv
 
 
 if __name__ == "__main__":
-    clean_campaign_data()
+    client_csv, campaign_csv, economics_csv = clean_campaign_data()
+    print("Client Data:")
+    print(client_csv.head())
+    print("\nCampaign Data:")
+    print(campaign_csv.head())
+    print("\nEconomics Data:")
+    print(economics_csv.head())
